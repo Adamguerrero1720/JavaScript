89d4894dd7c2db32977e87cadf946b43a89346df
Adam Guerrero <adamguerrrero2021@gmail.com>
2023-01-24 02:30:29 -0400

    24/1/2023

--- Programa_1.js
+++ Programa_1.js
@@ -1,1 +1,20 @@
-console.log("Hola Mundo");
+//Tipo de Datos
+
+console.log("Hola Mundo")
+
+30
+
+"asdnjk"
+
+true
+false
+
+//Variables
+
+const nombre = "Adam";
+
+var edad;
+
+edad = 30;
+
+console.log(nombre, edad);
